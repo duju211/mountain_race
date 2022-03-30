80c388299176d3136e3e86141f25183137725533
Julian During <duringjulian@gmail.com>
2022-03-30 16:03:17 +0000

    Improve wrong direction

--- R/wrong_direction.R
+++ R/wrong_direction.R
@@ -1,6 +1,11 @@
 wrong_direction <- function(df_lochen) {
-  df_lochen |>
+  df_lat_lng_start <- df_lochen |>
     group_by(id) |>
-    summarise(start_lat = lat[time == min(time)], start_lng = lng[time == min(time)]) |>
-    filter(start_lat == min(start_lat))
+    summarise(
+      start_lat = lat[time == min(time)], start_lng = lng[time == min(time)])
+  
+  midway <- min(df_lochen$lat) + (max(df_lochen$lat) - min(df_lochen$lat)) / 2
+  
+  df_lat_lng_start |>
+    filter(start_lat < midway)
 }
